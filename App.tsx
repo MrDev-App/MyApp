3537aaed5402c3d5e7bdde60af966369032a33f6
Developer <developer@users.noreply.github.com>
2026-08-20 11:09:54 +0530

    rename the src forlder

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'react-native';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import StackNavigation from './src2/navigation/StackNavigation';
+import StackNavigation from './src/navigation/StackNavigation';
 
 const App = () => {
   return (
